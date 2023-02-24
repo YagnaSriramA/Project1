25ba98a5e5cd3ed72251cf217bdf1e1833166694
Yagna Amballa <yagna.s.amballa@kipi.bi>
2023-02-24 17:55:08 +0530

    first upload of all the files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import './style.css'
+
+
+
+function App()
+{
+    return (
+        <div className="container">
+            <Navbar/>
+            <Main/>
+        </div>
+    )
+}
+
+export default App
